38335759ad379e077064d01b28a1e482939681e7
Vaagh Baraseon <rubicosmo@proton.me>
2024-09-03 12:59:27 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import Groq from "groq-sdk";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
+
+export async function main() {
+  const chatCompletion = await getGroqChatCompletion();
+  console.log(chatCompletion.choices[0]?.message?.content || "");
+}
+
+export async function getGroqChatCompletion() {
+  return groq.chat.completions.create({
+    messages: [
+      {
+        role: "user",
+        content: "Explain the importance of fast language models",
+      },
+    ],
+    model: "llama3-8b-8192",
+  });
+}
+
+// Запуск основной функции
+main();
